e6919de8f84ebf58ffb3a5463d3e9b999d2ebb6b
Gregory Zbitnev <zbitnev@hotmail.com>
2015-04-22 12:37:23 +0300

    Реализован класс спрайта

---
+++ src/viewmodel/Sprite.java
@@ -0,0 +1,46 @@
+package viewmodel;
+
+import java.awt.Dimension;
+import java.awt.geom.Point2D;
+
+import model.Speed2D;
+
+/**
+ * Спрайт игрового объекта
+ */
+public class Sprite {
+
+	com.golden.gamedev.object.Sprite _sprite;
+	
+	public void update(long timeElapsed) {
+		_sprite.update(timeElapsed);
+	}
+	
+	public Speed2D getSpeed() {
+		return new Speed2D(_sprite.getHorizontalSpeed(), _sprite.getVerticalSpeed());
+	}
+
+	public void setSpeed(Speed2D speed) {
+		
+		if (speed == null) {
+			throw new NullPointerException();
+		}
+		_sprite.setSpeed(speed.x(), speed.y());
+	}
+	
+	public Point2D.Double getPosition() {
+		return new Point2D.Double(_sprite.getX(), _sprite.getY());
+	}
+	
+	public void setPosition(Point2D.Double position) {
+		
+		if (position == null) {
+			throw new NullPointerException();
+		}
+		_sprite.setLocation(position.x, position.y);
+	}
+	
+	public Dimension getSize() {
+		return new Dimension(_sprite.getWidth(), _sprite.getHeight());
+	}
+}
