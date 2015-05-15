e99501eb12797298230ce3958e96363785ec1024
Gregory Zbitnev <zbitnev@hotmail.com>
2015-05-15 16:55:41 +0300

    Устаревшая фабрика представлений игровых объектов удалена
    Она больше не используется, вместо неё написана новая

--- src/arkanoid/FieldViewFactory.java
+++ src/arkanoid/FieldViewFactory.java
@@ -3,21 +3,17 @@ package arkanoid;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-
 import com.golden.gamedev.engine.BaseLoader;
-import com.golden.gamedev.object.background.ImageBackground;
 
-import arkanoid.entities.DefaultObjectViewFactory;
-import arkanoid.entities.ball.BasicBall;
-import arkanoid.entities.brick.BreakableBrick;
-import arkanoid.entities.brick.UnbreakableBrick;
-import arkanoid.entities.paddle.BasicPaddle;
+import arkanoid.entities.EntityViewFactory;
+
 
 /**
  * Фабрика представлений игрового поля.
  */
 public class FieldViewFactory {
 
+	private EntityViewFactory _factory = new EntityViewFactory();
 	/**
 	 * Получить представления для экземпляра игрового поля.
 	 * @param field Экземпляр игрового поля.
@@ -27,29 +23,8 @@ public class FieldViewFactory {
 		
 		ArkanoidFieldView newFieldView = new ArkanoidFieldView();
 		
-		// Загрузка изображений для представлений игровых объектов
-		BufferedImage bgImage               = bsLoader.getImage("default/gfx/misc/bg-blue.png");
-	    BufferedImage basicBallImage        = bsLoader.getImage("default/gfx/balls/basic.png");
-	    BufferedImage breakableBrickImage   = bsLoader.getImage("default/gfx/bricks/breakable.png");
-	    BufferedImage unbreakableBrickImage = bsLoader.getImage("default/gfx/bricks/unbreakable.png");
-	    BufferedImage basicPaddleImage      = bsLoader.getImage("default/gfx/paddles/basic.png");
-		DefaultObjectViewFactory viewfact = new DefaultObjectViewFactory(basicBallImage, breakableBrickImage,
-		        unbreakableBrickImage, basicPaddleImage, newFieldView);
-		
 		for (Entity e : field.getEntities()) {
-			
-			if (e instanceof BasicBall) {
-				newFieldView.addObjectView(viewfact.newBasicBallView((BasicBall)e));
-			}
-			else if (e instanceof BreakableBrick) {
-				newFieldView.addObjectView(viewfact.newBreakableBrickView((BreakableBrick)e));
-			}
-			else if (e instanceof UnbreakableBrick) {
-				newFieldView.addObjectView(viewfact.newUnbreakableBrickView((UnbreakableBrick)e));
-			}
-			else if (e instanceof BasicPaddle) {
-				newFieldView.addObjectView(viewfact.newBasicPaddleView((BasicPaddle)e));
-			}
+			newFieldView.addObjectView(_factory.instantiateEntityView(e, newFieldView));
 		}
 		
 		// Задать фон уровня.
--- src/arkanoid/entities/EntityViewFactory.java
+++ src/arkanoid/entities/EntityViewFactory.java
@@ -8,9 +8,12 @@ import java.util.Map;
 
 import javax.imageio.ImageIO;
 
+import arkanoid.ArkanoidFieldView;
 import arkanoid.Entity;
+import arkanoid.PublishingSprite;
 import arkanoid.entities.ball.BasicBall;
 import arkanoid.entities.brick.BreakableBrick;
+import arkanoid.entities.brick.UnbreakableBrick;
 import arkanoid.entities.paddle.BasicPaddle;
 
 /**
@@ -22,22 +25,26 @@ public class EntityViewFactory {
 		= new HashMap<Class<?>, String>();
 	static {
 		entityImageUrl.put(BreakableBrick.class, "default/gfx/bricks/breakable.png");
-		entityImageUrl.put(BreakableBrick.class, "default/gfx/bricks/unbreakable.png");
+		entityImageUrl.put(UnbreakableBrick.class, "default/gfx/bricks/unbreakable.png");
 		entityImageUrl.put(BasicBall.class, "default/gfx/balls/basic.png");
 		entityImageUrl.put(BasicPaddle.class, "default/gfx/paddles/basic.png");
 	}
 	
-	public EntityView instantiateEntityView(Entity entity) {
-		
-		EntityView view = new EntityView(entity.getSprite()._sprite, null, null, null);
+	public EntityView instantiateEntityView(Entity entity, ArkanoidFieldView fieldView) {
 		
 		BufferedImage img = null;
 		try {
 			img = ImageIO.read(new File(entityImageUrl.get(entity.getClass())));
 		} catch (IOException e) {
 		}
-		view._gtgeSprite.setImage(img);
 		
+		// TODO Убрать зависимость от игрового поля
+		// TODO Не использовать больше паблишинг спрайт
+		PublishingSprite pubSprite = new PublishingSprite();
+		pubSprite.setImage(img);
+		EntityView view = new EntityView(entity.getSprite()._sprite, entity, pubSprite, fieldView);
+		view._gtgeSprite.setImage(img);
+
 		return view;
 	}
 }
